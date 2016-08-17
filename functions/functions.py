581ae00acfc4634f1b554a80bb062f141aaac65f
Kwaw Annor <kwaw@kudobuzz.com>
2016-08-17 20:57:37 +0000

    Functions: basic rotate added

---
+++ functions/functions.py
@@ -0,0 +1,24 @@
+def rotate(seq, n):
+    """
+    Rotate seq
+
+    Args:
+        seq: iterables
+        n: point
+    Returns:
+        Rotated function
+    """
+
+    return seq[-n:] + seq[:-n]
+
+
+def lcm():
+    pass
+
+
+def find():
+    pass
+
+
+def compact():
+    pass
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,10 +1,41 @@
 import unittest
 
+from functions.functions import rotate, lcm, find, compact
+
 
 class FunctionsTestCase(unittest.TestCase):
 
     def test_rotate(self):
-        pass
+        l = tuple('abcde')
+        n = len(l)
+
+        r = rotate(l, 1)
+        self.assertEqual(''.join(r), 'eabcd')
+
+        r = rotate(l, -1)
+        self.assertEqual(''.join(r), 'bcdea')
+
+        for i in range(n):
+            e = l
+            d = rotate(l, i)
+            for j in range(i):
+                e = rotate(e, 1)
+            self.assertEqual(tuple(d), tuple(e))
+            d = rotate(d, -i)
+            self.assertEqual(tuple(d), l)
+            e = rotate(e, n - i)
+            self.assertEqual(tuple(e), l)
+
+        for i in range(n):
+            e = l
+            d = rotate(l, -i)
+            for j in range(i):
+                e = rotate(e, -1)
+            self.assertEqual(tuple(d), tuple(e))
+            d = rotate(d, i)
+            self.assertEqual(tuple(d), l)
+            e = rotate(e, i - n)
+            self.assertEqual(tuple(e), l)
 
     def test_lcm(self):
         pass
