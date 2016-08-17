78bf9215994159d182e71c1fea49cac5c491696c
Kwaw Annor <kwaw@kudobuzz.com>
2016-08-17 17:15:52 +0000

    Test: started

---
+++ tests/test_functions.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class FunctionsTestCase(unittest.TestCase):
+
+    def test_rotate(self):
+        pass
+
+    def test_lcm(self):
+        pass
+
+    def test_find(self):
+        pass
+
+    def test_compact(self):
+        pass
